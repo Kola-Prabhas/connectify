4c7c4a3deaf1aeeef21ebc1e65b8cba19aec60e7
Kola Prabhas <kolaprabhas2310@gmail.com>
2024-10-06 17:33:06 +0530

    Added real-time listeners for newly added messages

--- components/chat-input.tsx
+++ components/chat-input.tsx
@@ -1,49 +1,48 @@
 'use client';
 
 import { Input } from "@/components/ui/input"
-import { useMessages } from "@/lib/store/messages";
-import { useUser } from "@/lib/store/user";
+// import { useMessages } from "@/lib/store/messages";
+// import { useUser } from "@/lib/store/user";
 import { createClient } from "@/lib/supabase/client";
 import { toast } from "sonner";
 
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 
 
 function ChatInput() {
-	const addMessage = useMessages(state => state.addMessage);
-	const user = useUser(state => state.user);
+	// const addMessage = useMessages(state => state.addMessage);
+	// const user = useUser(state => state.user);
 
+	// No need to optimistically add message since we add it in real-time changes handler
 	async function handleSendMessage(message: string) {
 		const client = createClient();
 
 		const isMessageEmpty = !message;
 
-        // Don't allow empty messages and check for user is not undefined
-		if (!isMessageEmpty && user) {
-			const newMessage = {
-				created_at: new Date().toISOString(),
-				id: uuidv4(),
-				is_edit: false,
-				send_by: user?.id,
-				text: message,
-				users: {
-					avatar_url: user?.user_metadata.avatar_url,
-					created_at: user?.created_at,
-					display_name: user?.user_metadata.user_name,
-					id: user?.id ,
-				}
-			}
+        // Don't allow empty messages 
+		if (!isMessageEmpty) {
+			// const newMessage = {
+			// 	created_at: new Date().toISOString(),
+			// 	id: uuidv4(),
+			// 	is_edit: false,
+			// 	send_by: user?.id,
+			// 	text: message,
+			// 	users: {
+			// 		avatar_url: user?.user_metadata.avatar_url,
+			// 		created_at: user?.created_at,
+			// 		display_name: user?.user_metadata.user_name,
+			// 		id: user?.id ,
+			// 	}
+			// }
 
-			addMessage(newMessage);
+			// addMessage(newMessage);
 
 			const { error } = await client.from('messages').insert({ text: message });
 
 			if (error) {
 				toast.error(error.message);
 			}
-		}
-
-		if (isMessageEmpty) {
+		} else {
 			toast.error("Please enter a message!")
 		}	
 	}
--- components/list-messages.tsx
+++ components/list-messages.tsx
@@ -4,14 +4,50 @@ import { useMessages } from "@/lib/store/messages";
 import Message from "./message";
 import { DeleteDialog } from "./delete-dialog";
 import { EditDialog } from "./edit-dialog";
+import { useEffect } from "react";
+import { createClient } from "@/lib/supabase/client";
 
 
 function ListMessages() {
 	const messages = useMessages(state => state.messages);
+	const addMessage = useMessages(state => state.addMessage);
 	const actionMessage = useMessages(state => state.actionMessage);
 
-	// TODO: Undo the optimistic updates when the request fails
+	// TODO: Realtime handlers when a message is deleted and edited
 
+	useEffect(() => {
+		const client = createClient();
+		const subscription = client
+			.channel('room1')
+			.on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'messages' }, async payload => {
+				const message = payload.new;
+				const userData = await client.from('users')
+					.select('id, avatar_url, created_at, display_name')
+					.eq('id', message.send_by);
+				
+				const users = userData.data?.[0];
+
+				if (users) {
+					addMessage({
+						id: message.id,
+						created_at: message.created_at,
+						is_edit: message.is_edit,
+						send_by: message.send_by,
+						text: message.text,
+						users
+					})
+				}
+			}).subscribe();
+		
+		
+		return () => {
+			subscription.unsubscribe();
+		}
+
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+
+	// TODO: Undo the optimistic updates when the request fails
 	// TODO: After a message is edited it's position shouldn't change and it shouldn't be the recent message
 
 	return (
